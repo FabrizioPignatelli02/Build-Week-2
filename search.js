fa088851e0d59b3e8470ee0ca1ad4b2df043160e
GianniCbd <giannicabiddu@gmail.com>
2023-11-16 16:56:30 +0100

    search resp

--- search.js
+++ search.js
@@ -1,84 +1,99 @@
-function tryToSearch() {
-  const input = document.getElementById("search");
+document.addEventListener("DOMContentLoaded", function () {
+  const cardContainer = document.getElementById("cardContainer");
+  const cards = document.querySelectorAll(".searchCard");
 
-  input.style.display = input.style.display === "none" || input.style.display === "" ? "block" : "none";
-}
-//
-const apiKey = "a5517754abmsheebe0b22e59947fp18c555jsn03bc96d636c4";
-const apiHost = "deezerdevs-deezer.p.rapidapi.com";
+  const search = document.getElementById("search");
 
-const searchInput = document.getElementById("search");
-const songList = document.getElementById("songList");
+  search.addEventListener("input", function () {
+    const searchTerm = search.value.toLowerCase();
 
-const searchSongs = async () => {
-  const query = searchInput.value.trim();
-  if (!query) return;
+    cards.forEach(function (card) {
+      const cardTitle = card.querySelector(".cardSearchTitle").textContent.toLowerCase();
 
-  try {
-    const res = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${query}`, {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": apiHost
+      if (searchTerm === "" || cardTitle.indexOf(searchTerm) !== -1) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
       }
     });
-    const result = await res.json();
+  });
 
-    if (!songList) {
-      console.error("Err");
-    } else {
-      songList.innerHTML = "";
-      result.data.forEach((song) => {
-        const listItem = document.createElement("li");
-        listItem.className = "song";
+  const apiKey = "a5517754abmsheebe0b22e59947fp18c555jsn03bc96d636c4";
+  const apiHost = "deezerdevs-deezer.p.rapidapi.com";
 
-        const imgContainer = document.createElement("div");
-        imgContainer.className = "image-container";
+  const searchInput = document.getElementById("search");
+  const songList = document.getElementById("songList");
 
-        // imgContainer.style.width = "120px"
-        // imgContainer.style.height = "120px"
-        // imgContainer.style.backgroundImage = song.artist.picture
-        // console.log(song.artist);
-        imgContainer.style = `width:120px;height:120px; margin:auto; background-image: url(${song.artist.picture}); background-position:center; ;background-size:cover ;background-repeat:no-repeat`;
+  const searchSongs = async () => {
+    const query = searchInput.value.trim();
+    if (!query) return;
 
-        const textContainer = document.createElement("div");
-        textContainer.className = "text-container";
-
-        const h2 = document.createElement("h2");
-        const p1 = document.createElement("p");
-        const p2 = document.createElement("p");
-        p2.className = "smallParag";
-
-        h2.textContent = song.artist.name;
-        p1.textContent = song.album.title;
-        p2.textContent = song.title;
-
-        textContainer.appendChild(h2);
-        textContainer.appendChild(p1);
-        textContainer.appendChild(p2);
-
-        const logoContainer = document.createElement("div");
-        logoContainer.className = "logo-container";
-
-        const logo = document.createElement("img");
-        logo.src = "./assets/imgs/logo.svg";
-        logo.alt = "logo.svg";
-
-        logoContainer.appendChild(logo);
-
-        imgContainer.appendChild(logoContainer);
-        listItem.appendChild(imgContainer);
-        listItem.appendChild(textContainer);
-
-        songList.appendChild(listItem);
+    try {
+      const res = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${query}`, {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": apiKey,
+          "X-RapidAPI-Host": apiHost
+        }
       });
-    }
-  } catch (err) {
-    console.error("Error fetching data:", err);
-  }
-};
+      const result = await res.json();
 
-searchInput.addEventListener("input", searchSongs);
+      if (!songList) {
+        console.error("Error: songList not found");
+      } else {
+        songList.innerHTML = "";
+        result.data.forEach((song) => {
+          const listItem = document.createElement("li");
+          listItem.className = "song";
+
+          const imgContainer = document.createElement("div");
+          imgContainer.className = "image-container";
+          imgContainer.style = `width:120px;height:120px; margin:auto; background-image: url(${song.artist.picture}); background-position:center; ;background-size:cover ;background-repeat:no-repeat`;
+
+          const textContainer = document.createElement("div");
+          textContainer.className = "text-container";
+
+          const h2 = document.createElement("h2");
+          const p1 = document.createElement("p");
+          const p2 = document.createElement("p");
+          p2.className = "smallParag";
+
+          h2.textContent = song.artist.name;
+          p1.textContent = song.album.title;
+          p2.textContent = song.title;
+
+          textContainer.appendChild(h2);
+          textContainer.appendChild(p1);
+          textContainer.appendChild(p2);
+
+          const logoContainer = document.createElement("div");
+          logoContainer.className = "logo-container";
+
+          const logo = document.createElement("img");
+          logo.src = "./assets/imgs/logo.svg";
+          logo.alt = "logo.svg";
+
+          logoContainer.appendChild(logo);
+
+          imgContainer.appendChild(logoContainer);
+          listItem.appendChild(imgContainer);
+          listItem.appendChild(textContainer);
+
+          songList.appendChild(listItem);
+        });
+      }
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
+
+  searchInput.addEventListener("input", searchSongs);
+
+  function tryToSearch() {
+    const input = document.getElementById("search");
+    input.style.display = input.style.display === "none" || input.style.display === "" ? "block" : "none";
+  }
+});
 
 // color random
 
@@ -96,26 +111,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
   cards.forEach(function (card) {
     card.style.backgroundColor = getRandomColor();
-  });
-});
-
-// display-none search.html
-document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".searchCard");
-
-  var search = document.getElementById("search");
-
-  search.addEventListener("input", function () {
-    const searchTerm = search.value.toLowerCase();
-
-    cards.forEach(function (card) {
-      const cardTitle = card.querySelector(".cardSearchTitle").textContent.toLowerCase();
-
-      if (cardTitle.includes(searchTerm)) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
   });
 });
